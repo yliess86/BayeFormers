713acb740d0dec20a4d2879b3ebe0f40ea6ffec8
yliess86 <hatiyliess86@gmail.com>
2020-07-30 12:17:07 +0200

    Fixed Scale issue

--- examples/bert_glue.py
+++ examples/bert_glue.py
@@ -322,8 +322,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     hypersearch = HyperSearch()
-    hypersearch["DELTA"] = (1e-2, 1e-1)
-    hypersearch["WEIGHT_DECAY"] = (1e-3, 0)
+    hypersearch["DELTA"] = ((1e-2, 1e-1), True)
+    hypersearch["WEIGHT_DECAY"] = ((1e-3, 0), False)
     
     score = hypersearch.search(
         train, iterations=10,
